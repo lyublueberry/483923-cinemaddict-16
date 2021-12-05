9f9bd9b5a67fda0745349f5a5f4b573648cbab26
lyublueberry <lara.87@bk.ru>
2021-12-05 16:54:34 +0400

    небольшая правка

--- src/mock/comments.js
+++ src/mock/comments.js
@@ -1,13 +1,12 @@
-import { generateRandomElement, dateFilm } from '../util';
+import { generateRandomElement, generateDate } from '../util';
 
 const COMMENT_TEXT = ['good film', 'bad film', 'evil film'];
 const COMMENT_EMOTIONS = ['./images/emoji/angry.png', './images/emoji/sleeping.png', './images/emoji/puke.png', './images/emoji/angry.png'];
 const COMMENT_AUTHOR = ['JOHN DOE', 'JANE DOE', 'JOHN JANE'];
-const formatDate = 'YYYY/MM/DD HH:mm';
 
 export const generateCommentFilm = () => ({
   text: generateRandomElement(COMMENT_TEXT),
   emotion: generateRandomElement(COMMENT_EMOTIONS),
   author: generateRandomElement(COMMENT_AUTHOR),
-  date: dateFilm(formatDate), //«2019/12/31 23:59»
+  date: generateDate(), //«2019/12/31 23:59»
 });
